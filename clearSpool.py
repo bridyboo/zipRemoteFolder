14d69e58cc2a52fc2225adbfd5ab2c53abb676b9
bridyboo <57050319+bridyboo@users.noreply.github.com>
2023-09-26 21:32:56 -0700

    updated the client side to include archiveall

--- clearSpool.py
+++ clearSpool.py
@@ -4,17 +4,35 @@ import pwinput
 
 
 path = input("please add path for spool directory: ")
-prefix = input("what prefix are you looking to archive? ")
+choice = input("archive all (a) or prefix (b)? ")
+input("\n\n")
 
-if not path.startswith(r'\\'):
-    username = input("please provide username: ")
-    password = pwinput.pwinput()
-    server = input("please provide server address: ")
-    remote = remoteArchive.RemoteArchive(server, username, password)
-    remote.archive(prefix)
-    input()
+if choice == 'a':
+    prefix = input("what prefix are you looking to archive? ")
+
+    if not path.startswith(r'\\'):
+        username = input("please provide username: ")
+        password = pwinput.pwinput()
+        server = input("please provide server address: ")
+        remote = remoteArchive.RemoteArchive(server, username, password)
+        remote.archive(prefix)
+        input()
+
+    else:
+        sharedArchive = sharedPathArchive.SharedPathArchive(path)
+        sharedArchive.archive(prefix)
+        input()
 
 else:
-    sharedArchive = sharedPathArchive.SharedPathArchive(path)
-    sharedArchive.archive(prefix)
-    input()
+    if not path.startswith(r'\\'):
+        username = input("please provide username: ")
+        password = pwinput.pwinput()
+        server = input("please provide server address: ")
+        remote = remoteArchive.RemoteArchive(server, username, password)
+        remote.archiveAll()
+        input()
+
+    else:
+        sharedArchive = sharedPathArchive.SharedPathArchive(path)
+        sharedArchive.archiveAll()
+        input()
